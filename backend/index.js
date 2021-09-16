21d124afad9557f47ecab6b62317ed9e7e7bfcb8
Kat Leight <leightkt@gmail.com>
2021-09-16 17:44:56 -0600

    write initial routes, move db into separate folder

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,41 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-app.get('/', (req, res) => {
-    res.json({ "message": "hello" })
-})
+const database = require('./database/Database')
 
-app.get('/trails/:id', (req, res) => {
-    id = req.params.id
-    res.json({ "id": id })
+
+app.get('/trails', (req, res) => {
+    database('trails')
+        .then(trails => res.json(trails))
 })
 
 app.post('/trails', (req, res) => {
-    console.log(req.body)
-    const name = req.body.name
+    database('trails')
+        .returning(['id', 'name', 'url', 'thumbUrl', 'length', 'elevationGain', 'description'])
+        .insert({ name: req.body.name, url: req.body.url, thumbUrl: req.body.thumbUrl, length: req.body.length, elevationGain: req.body.elevationGain, description: req.body.description})
+        .then(trail => res.json(trail[0]))
 })
+
+app.get('/trails/:id', (req, res) => {
+    trail_id = req.params.id
+    database('trails')
+        .where({ id: trail_id})
+        .then(trail => res.json(trail[0]))
+})
+
+app.put('/trails/:id', (req, res) => {
+    database('trails')
+        .where({ id: req.params.id })
+        .update(req.body)
+        .then(trail => res.json(trail[0]))
+})
+
+app.delete('/trails/:id', (req, res) => {
+    database('trails')
+        .where({ id: req.params.id })
+        .del()
+        .then(response => res.json({"message": `trail with ${id} successfully deleted`}))
+})
+
 
 app.listen(port, () => console.log(`listening on port ${port}`))
--- backend/seeds/01-add-trails.js
+++ backend/seeds/01-add-trails.js
@@ -9,22 +9,22 @@ exports.seed = function(knex) {
           name: 'Bierstadt',
           url: 'https://www.14ers.com/route.php?route=bier1&peak=Mt.+Bierstadt',
           thumbUrl: 'https://www.14ers.com/photos/evansgroup/rt_bier1.jpg',
-          length: '7',
-          elevationGain: '2850',
+          length: 7,
+          elevationGain: 2850,
           description: 'Mount Bierstadt is a 14,065-foot-high mountain summit in the Front Range of the Rocky Mountains, in the U.S. state of Colorado. The fourteener is located in the Mount Evans Wilderness of Pike National Forest, 9.4 miles south by east of the Town of Georgetown in Clear Creek County.'
         }, {
           name: 'Shavano',
           url: 'https://www.14ers.com/route.php?route=shav1',
           thumbUrl: 'https://www.14ers.com/photos/shavanogroup/rt_shav1.jpg',
-          length: '9',
+          length: 9,
           elevationGain: '4600',
           description: 'Mount Shavano is a high mountain summit in the southern Sawatch Range of the Rocky Mountains of North America. The 14,231-foot fourteener is located in San Isabel National Forest, 6.5 miles north by west of the community of Maysville in Chaffee County, Colorado, United States.'
         }, {
           name: 'Handies',
           url: 'https://www.14ers.com/route.php?route=hand1',
           thumbUrl: 'https://www.14ers.com/photos/handiespeak/rt_hand1.jpg',
-          length: '7.5',
-          elevationGain: '2800',
+          length: 7.5,
+          elevationGain: 2800,
           description: 'Handies Peak is a high and prominent mountain summit of the San Juan Mountains range in the Rocky Mountains of North America.'
         }
       ]);
